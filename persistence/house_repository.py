05666a67a546a0279c36f2059607cc7f4d3fc998
CsSzegleti <szegleti_csongor@4dsoft.hu>
2023-08-23 18:01:07 +0200

    list houses

--- persistence/house_repository.py
+++ persistence/house_repository.py
@@ -1,16 +1,24 @@
 import sqlite3
 
-from config import config
+from business_logic.model.house import House
 
 
 class HouseRepository:
-    def list_houses(self):
-        conn = sqlite3.connect("database/" + config.database)
+
+    def __init__(self, database_name: str):
+        self.database_name = database_name
+
+    def list_houses(self) -> [House]:
+        conn = sqlite3.connect("database/hp.db")
         cursor = conn.cursor()
         sql = '''select ID, NAME, POINTS from houses'''
         cursor.execute(sql)
 
-
+        houses: [House] = []
+        for row in cursor:
+            houses.append(House(*row))
 
         cursor.close()
         conn.close()
+
+        return houses
