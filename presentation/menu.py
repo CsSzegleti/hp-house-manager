23eb374ed65961d91c3df9f8ea004bd58a0918ff
CsSzegleti <szegleti_csongor@4dsoft.hu>
2023-08-30 21:11:38 +0200

    pass arguments to menu items

--- presentation/menu.py
+++ presentation/menu.py
@@ -1,17 +1,23 @@
 import os
 
 
+class FunctionWithArgs:
+    def __init__(self, func: callable, args):
+        self.func = func
+        self.args = args
+
+
 class Menu:
     def __init__(self, name: str):
         self.isActive = False
         self.items = dict()
         self.name = name
 
-    def add_menu_item(self, title: str, func: callable):
+    def add_menu_item(self, title: str, func: callable, *args):
         if title in self.items.keys():
             raise Exception("error.item.already.exist")
 
-        self.items[title] = func
+        self.items[title] = FunctionWithArgs(func, args)
 
     def draw_menu(self):
         os.system("clear")
@@ -37,7 +43,7 @@ class Menu:
             if correct:
                 for idx, title in enumerate(self.items):
                     if idx + 1 == selected_menu_item:
-                        self.items[title]()
+                        self.items[title].func(*self.items[title].args)
                         break
 
     def deactivate(self):
